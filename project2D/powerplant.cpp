46aacf5dd72cb5689e242a0f505576e72f6857fa
Cameron Brown <cameron@cmrn.io>
2018-03-16 13:18:58 +1100

    power plants blink and smile now :D

--- project2D/powerplant.cpp
+++ project2D/powerplant.cpp
@@ -4,6 +4,7 @@
 #include "tile.h"
 #include "game.h"
 #include "imagemanager.h"
+#include "random.h"
 
 PowerPlant::PowerPlant(Game* game, int x, int y)
 	: Building(game, x, y)
@@ -17,6 +18,38 @@ PowerPlant::PowerPlant(Game* game, int x, int y)
 	m_price = 1000;
 
 	m_texture = game->getImageManager()->getTexture("buildings/powerplant");
+	
+	// eyes and mouth stuff
+	m_blinking = false;
+	m_mouthOpen = false;
+	m_blinkTimer = 0;
+
+	m_openMouth = game->getImageManager()->getTexture("mouth_open");
+	m_closedMouth = game->getImageManager()->getTexture("mouth_closed");
+}
+
+void PowerPlant::update(float delta)
+{
+	Building::update(delta);
+
+	m_blinkTimer -= delta;
+
+	const float blinkLength = 0.1f;
+
+	if (m_blinkTimer <= 0)
+	{
+		m_blinking = !m_blinking;
+		if (m_blinking)
+			m_blinkTimer = blinkLength;
+		else
+			m_blinkTimer = randBetween(1.0f, 10.0f);
+	}
+
+	Vector2 mousePos = m_game->getMouseWorldPosition();
+	Vector2 thisPos = m_worldPos + Vector2(0, 256);
+	float dist = mousePos.distanceToSquared((thisPos));
+
+	m_mouthOpen = dist < 350*350;
 }
 
 void PowerPlant::draw(aie::Renderer2D* renderer)
@@ -32,8 +65,17 @@ void PowerPlant::draw(aie::Renderer2D* renderer)
 		m_worldPos.getY() - 4 + m_altitude, 0, 0, 0, 0, xOrigin, yOrigin);
 
 	// eyeballs
-	drawEyeball(renderer, Vector2(m_worldPos.getX() + 84.0f, 
-		m_worldPos.getY() + 128.0f + m_altitude), 48.0f);
-	drawEyeball(renderer, Vector2(m_worldPos.getX() + 194.0f, 
-		m_worldPos.getY() + 184.0f + m_altitude), 48.0f);
+	if (!m_blinking)
+	{
+		drawEyeball(renderer, Vector2(m_worldPos.getX() + 84.0f,
+			m_worldPos.getY() + 148.0f + m_altitude), 48.0f);
+		drawEyeball(renderer, Vector2(m_worldPos.getX() + 194.0f,
+			m_worldPos.getY() + 204.0f + m_altitude), 48.0f);
+	}
+
+	// draw mouth
+	aie::Texture* mtex = m_mouthOpen ? m_openMouth : m_closedMouth;
+	renderer->setRenderColour(1, 1, 1);
+	renderer->drawSprite(mtex, m_worldPos.getX() + 161.0f, 
+		m_worldPos.getY() + 120.0f + m_altitude);
 }
--- project2D/powerplant.h
+++ project2D/powerplant.h
@@ -7,5 +7,13 @@ class PowerPlant : public Building
 public:
 	PowerPlant(Game* game, int x, int y);
 
+	void update(float delta) override;
 	virtual void draw(aie::Renderer2D* renderer) override;
+private:
+	aie::Texture* m_openMouth;
+	aie::Texture* m_closedMouth;
+
+	bool m_blinking;
+	bool m_mouthOpen;
+	float m_blinkTimer;
 };
