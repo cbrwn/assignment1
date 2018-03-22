2b551a2c5588038bff60faa5f8f4cc6f06eeb9c6
Cameron Brown <cameron@cmrn.io>
2018-03-22 14:27:28 +1100

    clean up and comment DArray

--- project2D/darray.h
+++ project2D/darray.h
@@ -1,6 +1,8 @@
+/*
+	DArray - Dynamic Array
+	Simple replacement for std::vector
+*/
 #pragma once
-
-#include <cstdlib>
 
 #define DARRAY_DEFAULT_SIZE 8
 
@@ -15,15 +17,15 @@ public:
 
 		m_items = new T[m_size];
 	}
-	~DArray()
-	{
-		delete[] m_items;
-	}
+	~DArray() { delete[] m_items; }
+
+	// todo: copy/move constructors when i'm not so lazy
 
 	T& operator[](int index) { return m_items[index]; }
 
 	void add(const T& item)
 	{
+		// just stick it on the end of the array
 		m_items[m_itemCount] = item;
 		m_itemCount++;
 
@@ -31,22 +33,26 @@ public:
 		if (m_itemCount >= m_size - 1)
 		{
 			m_size *= 2;
-			
+		
+			// make a whole new array with the new size
 			T* resized = new T[m_size];
+			// and copy all the stuff over
 			for (int i = 0; i < m_itemCount; ++i)
 				resized[i] = m_items[i];
 
+			// get rid of the old array
 			delete[] m_items;
+			// and make it point to the new one
 			m_items = resized;
-			// apparently realloc doesn't like to work
+			
+			// apparently realloc doesn't like to work :(
 			//m_items = (T*)realloc(m_items, m_size);
 		}
 	}
 
 	void remove(int index)
 	{
-		// if we're removing the last item in the array, we can just
-		//   decrease the count so it gets overridden next time we add
+		// removing the last item is just popping off the top
 		if (index == m_itemCount - 1)
 		{
 			pop();
@@ -63,6 +69,7 @@ public:
 		m_itemCount--;
 	}
 
+	// searches for item in the array and removes it if found
 	void remove(const T& item)
 	{
 		for (int i = 0; i < m_itemCount; ++i)
@@ -75,23 +82,19 @@ public:
 		}
 	}
 
-	void pop()
-	{
-		m_itemCount--;
-	}
-
-	void clear()
-	{
-		m_itemCount = 0;
-	}
+	// pop and clear don't actually do much
+	// they just change the item count so that unwanted items aren't
+	//   accessed and will be overwritten
+	void pop() { m_itemCount--; }
+	void clear() { m_itemCount = 0; }
 
 	inline int getCurrentSize() { return m_size; }
 	inline int getCount() { return m_itemCount; }
 
-	// shouldn't be used which is why the _ is here
+	// use subscript operator to get array elements, not this!
 	inline T* _getArray() { return m_items; }
 private:
 	T*		m_items;
 	int		m_size;
 	int		m_itemCount;
-};
+}; // exactly 100 lines looks nice
